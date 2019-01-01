185b2195bc85e05010b8c7173160e8cb681d2ea9
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-01-01 12:01:54 -0800

    clean and build hecho

--- build/generated-sources/ap-source-output/com/rrhh/domain/prod/Idiomas_.java
+++ build/generated-sources/ap-source-output/com/rrhh/domain/prod/Idiomas_.java
@@ -10,7 +10,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-12-31T00:12:32")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-01T11:57:56")
 @StaticMetamodel(Idiomas.class)
 public class Idiomas_ { 
 
--- build/generated-sources/ap-source-output/com/rrhh/domain/prod/NivelAcademico_.java
+++ build/generated-sources/ap-source-output/com/rrhh/domain/prod/NivelAcademico_.java
@@ -7,7 +7,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2018-12-31T00:12:32")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2019-01-01T11:57:56")
 @StaticMetamodel(NivelAcademico.class)
 public class NivelAcademico_ { 
 
