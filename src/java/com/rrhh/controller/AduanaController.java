b4dcbf2c810b59cf33b8bc60f9c4198dc69e2f35
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-01-02 21:23:39 -0800

    template admin done

--- src/java/com/rrhh/controller/AduanaController.java
+++ src/java/com/rrhh/controller/AduanaController.java
@@ -3,23 +3,23 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package com.rrhh.controller;
-
-import javax.inject.Named;
-import javax.faces.view.ViewScoped;
-
-/**
- *
- * @author DELL
- */
-@Named(value = "aduanaController")
-@ViewScoped
-public class AduanaController {
-
-    /**
-     * Creates a new instance of AduanaController
-     */
-    public AduanaController() {
-    }
-    
-}
+//package com.rrhh.controller;
+//
+//import javax.inject.Named;
+//import javax.faces.view.ViewScoped;
+//
+///**
+// *
+// * @author DELL
+// */
+//@Named(value = "aduanaController")
+//@ViewScoped
+//public class AduanaController {
+//
+//    /**
+//     * Creates a new instance of AduanaController
+//     */
+//    public AduanaController() {
+//    }
+//    
+//}
