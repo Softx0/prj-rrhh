004e99c643b3819fd7d220edb7df90be14c7e7a3
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-01-02 22:17:45 -0800

    created aduanas for login and access to app

--- src/java/com/rrhh/controller/AduanaController.java
+++ src/java/com/rrhh/controller/AduanaController.java
@@ -3,23 +3,49 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-//package com.rrhh.controller;
-//
-//import javax.inject.Named;
-//import javax.faces.view.ViewScoped;
-//
-///**
-// *
-// * @author DELL
-// */
-//@Named(value = "aduanaController")
-//@ViewScoped
-//public class AduanaController {
-//
-//    /**
-//     * Creates a new instance of AduanaController
-//     */
-//    public AduanaController() {
-//    }
-//    
-//}
+package com.rrhh.controller;
+
+import com.rrhh.domain.Usuario;
+import com.rrhh.helper.util.JsfUtil;
+import com.rrhh.helper.util.SessionReference;
+import java.io.IOException;
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author DELL
+ */
+@Named(value = "aduanaController")
+@ViewScoped
+public class AduanaController {
+
+    SessionReference sessionReference;
+    Usuario usuario;
+
+    /**
+     * Creates a new instance of AduanaController
+     */
+    public AduanaController() {
+    }
+
+    public void onlyAdmin() {
+        try {
+            sessionReference = new SessionReference();
+            usuario = new Usuario();
+
+            usuario = (Usuario) sessionReference.sessionMapGet("usuario");
+
+            if (usuario == null || !usuario.getTipoUsuario().getDescripcion().equals("ADMIN")) {
+                sessionReference.getFacesContext().getExternalContext().redirect("account/login.xhtml");
+            }
+
+        } catch (IOException e) {
+            
+            JsfUtil.addErrorMessage(e, "Ha ocurrido un error!" + e);
+        }
+    }
+    
+    
+
+}
