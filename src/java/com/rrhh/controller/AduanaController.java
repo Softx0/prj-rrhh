b8958d9d1cb794fefb2b792112c952222a5effad
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-01-02 19:25:06 -0800

    fixed

---
+++ src/java/com/rrhh/controller/AduanaController.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.rrhh.controller;
+
+import javax.inject.Named;
+import javax.faces.view.ViewScoped;
+
+/**
+ *
+ * @author DELL
+ */
+@Named(value = "aduanaController")
+@ViewScoped
+public class AduanaController {
+
+    /**
+     * Creates a new instance of AduanaController
+     */
+    public AduanaController() {
+    }
+    
+}
