0aa4ae28e085fc3f99279d043878954eb2dd7270
Emmanuel Valenzuela <32858351+Softx0@users.noreply.github.com>
2019-01-03 09:02:59 -0800

    some bugs fixed

--- src/java/com/rrhh/controller/AduanaController.java
+++ src/java/com/rrhh/controller/AduanaController.java
@@ -41,11 +41,52 @@ public class AduanaController {
             }
 
         } catch (IOException e) {
-            
+
             JsfUtil.addErrorMessage(e, "Ha ocurrido un error!" + e);
         }
     }
-    
-    
+
+    public void onlyCandidato() {
+        try {
+            sessionReference = new SessionReference();
+            usuario = new Usuario();
+
+            usuario = (Usuario) sessionReference.sessionMapGet("usuario");
+
+            if (usuario == null || !usuario.getTipoUsuario().getDescripcion().equals("CANDIDATO")) {
+                sessionReference.getFacesContext().getExternalContext().redirect("./../acceso/login.xhtml");
+            }
+
+        } catch (IOException e) {
+            System.err.println("Ha ocurrido un error! " + e);
+        }
+    }
+
+    public void onlyEmpleado() {
+        try {
+            sessionReference = new SessionReference();
+            usuario = new Usuario();
+
+            usuario = (Usuario) sessionReference.sessionMapGet("usuario");
+
+            if (usuario == null || !usuario.getTipoUsuario().getDescripcion().equals("EMPLEADO")) {
+                sessionReference.getFacesContext().getExternalContext().redirect("./../acceso/login.xhtml");
+            }
+
+        } catch (IOException e) {
+            System.err.println("Ha ocurrido un error! " + e);
+        }
+    }
+
+    public void logOut() {
+
+        try {
+            sessionReference = new SessionReference();
+            sessionReference.sessionMapGetRemove("usuario");
+
+        } catch (Exception e) {
+            System.err.println("Ha ocurrido un error! " + e);
+        }
+    }
 
 }
